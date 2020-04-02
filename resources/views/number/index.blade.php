7ecf5a13754d4719534970f51cfa8c99d99a7f30
Chavalit Koweerawong <chavalit.kow@gmail.com>
2020-04-02 20:55:13 +0700

    Update index.blade.php

--- resources/views/number/index.blade.php
+++ resources/views/number/index.blade.php
@@ -14,7 +14,7 @@
                                 <div class="form-group col-lg">
                                     <label for="">ค้นหาเบอร์</label>
                                     <div class="input-group">
-                                        <input type="text" class="form-control" name="search" placeholder="Search..." value="{{ request('search') }}">
+                                        <input type="text" class="form-control" name="search" placeholder="Search..." value="{{ request('search') }}" autocomplete="off">
                                         <span class="input-group-append">
                                             <button class="btn btn-secondary" type="submit">
                                                 <i class="fa fa-search"></i>
